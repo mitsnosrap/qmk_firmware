53722063760a8439398e9ec4cb01f84489478db8
Tim Parsons <tim@oviahealth.com>
2020-12-28 15:12:13 -0500

    Pawmac keyboard files

---
+++ keyboards/mitsnosrap/pawmac/keymaps/mitsnosrap/keymap.c
@@ -0,0 +1,43 @@
+/* Copyright 2019 Tim Parsons
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include QMK_KEYBOARD_H
+#include "mitsnosrap.h"
+
+// Layer labels defined for readability
+#define _BL 0
+#define _FL 1
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [_BL] = LAYOUT(
+        _______, _______, KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,            KC_F9,   KC_F10,  KC_F11,  KC_F12,
+        M_GD,    M_GDC,   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_F12,
+        _______, _______, KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL,
+        _______, _______, KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP,
+        _______, _______, KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_PGDN,
+        _______, _______, KC_LCTL, KC_LALT, KC_LGUI,                            KC_SPC,                    KC_RGUI, KC_RALT, MO(_FL), KC_LEFT, KC_DOWN, KC_RGHT),
+
+    [_FL] = LAYOUT(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, M_GSTAT, M_GBRAN, M_GLOG,  M_GPULL, M_GPRUN, M_GCDEV, _______, _______, M_GDIFF, KC_MPLY, KC_MRWD, KC_MFFD, KC_MUTE, _______,
+        _______, _______, _______, M_GSLIS, M_GSSHO, M_GSAPL, M_GSDRO, M_GSPUS, _______, _______, _______, _______, _______, M_CLLOG,          _______, KC_F15,
+        _______, _______, _______, M_ACFER, M_ACPRE, M_ACPAR, M_AOTLS, M_AOALS, M_AORLS, _______, _______, _______, _______, _______,          KC_VOLU, KC_F14,
+        _______, _______, _______, _______, _______,                            _______,                   _______, _______, _______, KC_HOME, KC_VOLD, KC_END),
+};
+
+bool process_record_keymap(uint16_t keycode, keyrecord_t *record) {
+    return true;
+}
