2b2eb2cb8e69f2050061f00e694bae952e4d928b
Tim Parsons <tim@oviahealth.com>
2021-03-24 08:22:46 -0400

    Update keymap for Clueboard 66
    Remove the third layer that was only used for Zoom commands.

--- keyboards/clueboard/66/keymaps/mitsnosrap/keymap.c
+++ keyboards/clueboard/66/keymaps/mitsnosrap/keymap.c
@@ -7,7 +7,6 @@
 // entirely and just use numbers.
 #define _BL 0
 #define _FL 1
-#define _ML 2
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* Keymap _BL: Base Layer (Default Layer)
@@ -17,7 +16,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,                   KC_DEL,
   KC_GESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,
   KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RO,   KC_RSFT,          KC_UP,
-  KC_LCTL, KC_LALT, KC_LGUI, _______,          KC_SPC,  KC_SPC,                    _______, KC_RCTL, _______, MO(_FL), KC_LEFT, KC_DOWN, KC_RGHT),
+  KC_LCTL, KC_LALT, KC_LGUI, _______,          KC_SPC,  KC_SPC,                    _______, KC_RCTL, MO(_FL), MO(_FL), KC_LEFT, KC_DOWN, KC_RGHT),
 
   /* Keymap _FL: Function Layer
    */
@@ -26,14 +25,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, M_GSTAT, M_GBRAN, M_GLOG,  M_GPULL, M_GPRUN, M_GCDEV, _______, _______, M_GDIFF, KC_MPLY, KC_MRWD, KC_MFFD, KC_MUTE,                   KC_PGDN,
   KC_CAPS, M_GSLIS, M_GSSHO, M_GSAPL, M_GSDRO, M_GSPUS, _______, _______, _______, M_GPGPW, M_PGPG,  M_CLLOG, _______, _______,
   _______, _______, M_ACFER, M_ACPRE, M_ACPAR, M_AOTLS, M_AOALS, M_AORLS, _______, KC_F14,  KC_F15,  _______, _______, _______, KC_VOLU,
-  _______, _______, _______, _______,          _______, _______,                   _______, _______, MO(_ML), MO(_FL), KC_HOME, KC_VOLD, KC_END),
+  _______, _______, _______, _______,          _______, _______,                   _______, _______, MO(_FL), MO(_FL), KC_HOME, KC_VOLD, KC_END),
 
-  /* Keymap _ML: Function Layer 2
-   */
-[_ML] = LAYOUT(
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          M_ZVID,
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   M_ZMUTE,
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-  _______, _______, _______, _______,          _______, _______,                   _______, _______, _______, _______, _______, _______, _______),
 };
